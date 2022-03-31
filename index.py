b09591964896277870f495380193aff7c32ffdce
Skyler Kerr <skykerr12@gmail.com>
2022-03-31 00:38:07 -0500

    Basic user commands and message reactions

---
+++ index.py
@@ -0,0 +1,37 @@
+import discord
+import json
+with open('config.json') as f:
+    config = json.load(f)
+
+print(f'Starting up: Skybot Version {config["currentVersion"]} build {config["currentBuild"]}')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message): # Message content responses
+    if message.author == client.user:
+        return
+
+    if 'spoopy' in message.content.lower():
+        await message.add_reaction('🚫')
+
+@client.event
+async def on_message(message): # User Command Responses
+    if message.author == client.user:
+        return
+
+    command = ""
+    if message.content.startswith(config['commandChar']):
+        command = message.content.replace(config['commandChar'], "").lower()
+
+    if command == 'ping':
+        message.channel.send('Pong!')
+
+    if command == 'version':
+        message.channel.send(f'Version {config["currentVersion"]}, Build {config["currentBuild"]}')
+
+client.run(config['token'])
