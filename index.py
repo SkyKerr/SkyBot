3f6c6885d2057f013ec5c29df94244deb4e285da
Skyler Kerr <skykerr12@gmail.com>
2022-03-31 00:39:03 -0500

    Rusty rusty rusty

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message): # Message content responses
