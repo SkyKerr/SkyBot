baa2f8a168b107d4ababd5774e2184c21c8efd04
Skyler Kerr <skykerr12@gmail.com>
2022-05-01 22:29:30 -0500

    ability to have non true-false settings

--- guildSettings/guildSettings.py
+++ guildSettings/guildSettings.py
@@ -2,6 +2,7 @@ from index import *
 with open('guildSettings/descriptions.json') as f:
 	settingDescriptions = json.load(f)	
 
+# Note: As of v0.4.1 there is no way to add new settings to existing guilds. For testing in v0.5pre, guild settings *for the testing guild only* should be deleted so that they can be reloaded.
 def guildSettings(message):
 	guildID = message.channel.guild.id
 	filePath = (f'guildSettings/guilds/{guildID}.json')
@@ -19,11 +20,10 @@ def setSetting(message, settings):
 	with open(filePath, 'w') as f:
 		f.write(json.dumps(settings))
 
-
 async def settingsCommands(message, settings):
 	if not message.content.startswith(botInfo['commandChar']):
 		return
-	args = message.content.replace(botInfo['commandChar'], "").split(' ')
+	args = message.content.lstrip(botInfo['commandChar']).split(' ')
 
 	# Status command
 	if args[0] == 'status':
@@ -43,33 +43,70 @@ async def settingsCommands(message, settings):
 		
 			for setting in settings:
 				messageEmbed.add_field(
-				name=(f'{setting}: {settings[setting]}'),
-				value=(f'{settingDescriptions[setting]}'),
+				name=(f'{setting}'),
+				value=(f'{settingDescriptions[setting]["description"]}\n**Value:** {settings[setting]}\n**Type:** {settingDescriptions[setting]["type"]}'),
 				inline=False)
 		
 			await message.channel.send(embed=messageEmbed)
 			return
 		
+		if args[1] not in settings:
+			await message.channel.send('Setting not found')
+			return
+		
 		# Change settings commands
-		if args[1] in settings:
-			if len(args)==2:
-				await message.channel.send(f'{args[1]}: {settings[args[1]]}')
+		if len(args)==2:
+			await message.channel.send(f'{args[1]}: {settings[args[1]]}')
+			return
+		else:
+			if not goodType(args[1], args[2]):
+				await message.channel.send('Error: Input does not match setting type')
 				return
-			else:
-				if not message.author.guild_permissions.manage_messages:
-					await message.channel.send('You do not have the permissions to change the settings')
-					return
-				
+			
+			if not message.author.guild_permissions.manage_messages:
+				await message.channel.send('You do not have the permissions to change the settings')
+				return
+			
+			if settingDescriptions[args[1]]['type'] == 'bool':
 				if args[2].lower() == 'true':
 					settings[args[1]] = True
-					setSetting(message, settings)
-					await message.channel.send(f'{args[1]} set to True')
 				if args[2].lower() == 'false':
 					settings[args[1]] = False
-					setSetting(message, settings)
-					await message.channel.send(f'{args[1]} set to False')
+			if settingDescriptions[args[1]]['type'] == 'int':
+				settings[args[1]] = int(args[2])
+			else:
+				settings[args[1]] = args[2]
+			
+			setSetting(message, settings)
+			await message.channel.send(f'{args[1]} set to {args[2]}')
+
+def goodType(setting, input):
+	settingType = settingDescriptions[setting]['type']
+	
+	if settingType == 'bool':
+		if input.lower() == ('true' or 'false'):
+			return True
 		else:
-			await message.channel.send('Setting not found')
-
-
+			return False
+	
+	if settingType == 'char':
+		if len(input) != 1:
+			return False
+		if input in settingDescriptions['forbiddenCharacters']: # This is for all the amazing testers who do whatever they can to break my bot
+			return False
+		return True
+	
+	if settingType == 'str': 
+		for char in input.split(''):
+			if char in settingDescriptions['forbiddenCharacters']:
+				return False
+		return True
+	
+	if settingType == 'int':
+		try:
+			int(input)
+		except ValueError:
+			return False
+		return True
+		
 	
