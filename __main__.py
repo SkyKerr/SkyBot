043fb3ef4976f2f57bbfc1dd12eb03159979bd83
Ben Cardy <ben.cardy@ocado.com>
2022-06-17 19:34:57 +0100

    Blackened code

--- __main__.py
+++ __main__.py
@@ -2,34 +2,38 @@ from index import *
 from commands import *
 from guildSettings.guildSettings import *
 
-print(f'Starting up: Skybot {version}')
+print(f"Starting up: Skybot {version}")
+
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(f'({version}) {botInfo["statusMessage"]}'))
-    print('logged in as {0.user}\n\n'.format(client))
+    await client.change_presence(
+        activity=discord.Game(f'({version}) {botInfo["statusMessage"]}')
+    )
+    print("logged in as {0.user}\n\n".format(client))
+
 
 @client.event
-async def on_message(message): 
+async def on_message(message):
     settings = guildSettings(message)
-    
+
     if message.author == client.user:
         return
-    
+
     await settingsCommands(message, settings)
-    if (not settings['enableBot']):
+    if not settings["enableBot"]:
         return
-    
-    if settings['allowMessageResponses']:
+
+    if settings["allowMessageResponses"]:
         await messageResponses(message)
 
-    if message.content.startswith(settings['commandChar']):
-        args = shlex.split(message.content.lower().replace(settings['commandChar'],''))
+    if message.content.startswith(settings["commandChar"]):
+        args = shlex.split(message.content.lower().replace(settings["commandChar"], ""))
         command = args.pop(0)
-        
+
         await userCommands(message, command, args, settings)
-        if settings['allowJapeCommands']:
+        if settings["allowJapeCommands"]:
             await japeCommands(message, command, args, settings)
 
 
-client.run(config['token'])
+client.run(config["token"])
